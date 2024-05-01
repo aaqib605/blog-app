0fa595a89b96bac4f46508b1e06d01f34ed63fe7
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-05-01 16:43:46 +0530

    feat: add animation to signin / signup pages

--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -1,72 +1,75 @@
 import { Link } from "react-router-dom";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
+import AnimationWrapper from "../common/page-animation";
 
 const UserAuthForm = ({ type }) => {
   return (
-    <section className="h-cover flex items-center justify-center">
-      <form className="w-[80%] max-w-[400px]">
-        <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-          {type === "Sign In" ? "Welcome Back" : "Join Us Today!"}
-        </h1>
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex items-center justify-center">
+        <form className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type === "Sign In" ? "Welcome Back" : "Join Us Today!"}
+          </h1>
 
-        {type === "Sign Up" ? (
+          {type === "Sign Up" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Full Name"
+              icon="fi-rr-user"
+            />
+          ) : (
+            ""
+          )}
+
           <InputBox
-            name="fullname"
-            type="text"
-            placeholder="Full Name"
-            icon="fi-rr-user"
+            name="email"
+            type="email"
+            placeholder="Email"
+            icon="fi-rr-envelope"
           />
-        ) : (
-          ""
-        )}
 
-        <InputBox
-          name="email"
-          type="email"
-          placeholder="Email"
-          icon="fi-rr-envelope"
-        />
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Password"
+            icon="fi-rr-key"
+          />
 
-        <InputBox
-          name="password"
-          type="password"
-          placeholder="Password"
-          icon="fi-rr-key"
-        />
+          <button type="submit" className="btn-dark center mt-14">
+            {type}
+          </button>
 
-        <button type="submit" className="btn-dark center mt-14">
-          {type}
-        </button>
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <span>or</span>
+            <hr className="w-1/2 border-black" />
+          </div>
 
-        <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-          <hr className="w-1/2 border-black" />
-          <span>or</span>
-          <hr className="w-1/2 border-black" />
-        </div>
+          <button className="btn-dark flex justify-center items-center gap-4 w-[90%] center">
+            <img src={googleIcon} alt="Google Icon" className="w-5" />
+            continue with google
+          </button>
 
-        <button className="btn-dark flex justify-center items-center gap-4 w-[90%] center">
-          <img src={googleIcon} alt="Google Icon" className="w-5" />
-          continue with google
-        </button>
-
-        {type === "Sign In" ? (
-          <p className="mt-6 text-dark-grey text-xl text-center">
-            Don't have an account?
-            <Link to="/signup" className="underline text-black text-xl ml-1">
-              Join us today
-            </Link>
-          </p>
-        ) : (
-          <p className="mt-6 text-dark-grey text-xl text-center">
-            Already a member?
-            <Link to="/signin" className="underline text-black text-xl ml-1">
-              Sign in here
-            </Link>
-          </p>
-        )}
-      </form>
-    </section>
+          {type === "Sign In" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Don't have an account?
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+                Join us today
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Already a member?
+              <Link to="/signin" className="underline text-black text-xl ml-1">
+                Sign in here
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </AnimationWrapper>
   );
 };
 
