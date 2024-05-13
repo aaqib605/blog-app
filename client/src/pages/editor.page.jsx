1bca198cf047d9481d5f866699cfd65c54147bd8
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-05-13 13:18:48 +0530

    feat: setup blog editor navbar, banner

--- client/src/pages/editor.page.jsx
+++ client/src/pages/editor.page.jsx
@@ -1,5 +1,21 @@
+import { useContext, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../App";
+import BlogEditor from "../components/blog-editor.component";
+import PublishForm from "../components/publish-form.component";
+
 const Editor = () => {
-  return <div>Editor</div>;
+  const [editorState, setEditorState] = useState("editor");
+
+  const {
+    userAuth: { jwtToken },
+  } = useContext(UserContext);
+
+  if (!jwtToken) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  return editorState === "editor" ? <BlogEditor /> : <PublishForm />;
 };
 
 export default Editor;
