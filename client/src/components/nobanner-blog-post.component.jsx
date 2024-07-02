a2e3c9e0e31da9f106c45af69fe63f8be3715035
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-07-02 15:41:11 +0530

    feat: display trending blogs on UI

---
+++ client/src/components/nobanner-blog-post.component.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import { getDate } from "../common/date";
+
+const MinimalBlogPost = ({ blog, index }) => {
+  const {
+    title,
+    blogId: id,
+    author: {
+      personalInfo: { fullname, username, profileImg },
+    },
+    publishedAt,
+  } = blog;
+  return (
+    <Link to={`/blog/${id}`} className="flex gap-5 mb-8">
+      <h1 className="blog-index">{index < 10 ? `0${index + 1}` : index + 1}</h1>
+
+      <div>
+        <div className="flex gap-2 items-center mb-7">
+          <img
+            src={profileImg}
+            alt="User profile"
+            className="w-6 h-6 rounded-full"
+          />
+          <p className="line-clamp-1">
+            {fullname} @{username}
+          </p>
+          <p className="min-w-fit">{getDate(publishedAt)}</p>
+        </div>
+
+        <h1 className="blog-title">{title}</h1>
+      </div>
+    </Link>
+  );
+};
+
+export default MinimalBlogPost;
--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -4,9 +4,11 @@ import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
 import BlogPostCard from "../components/blog-post.component";
 import Loader from "../components/loader.component";
+import MinimalBlogPost from "../components/nobanner-blog-post.component";
 
 const HomePage = () => {
-  const [blogs, setBlogs] = useState(null);
+  const [latestBlogs, setLatestBlogs] = useState(null);
+  const [trendingBlogs, setTrendingBlogs] = useState(null);
 
   const fetchLatestBlogs = async () => {
     try {
@@ -14,7 +16,21 @@ const HomePage = () => {
         data: { blogs },
       } = await axios.get(`${import.meta.env.VITE_SERVER_DOMAIN}/latest-blogs`);
 
-      setBlogs(blogs);
+      setLatestBlogs(blogs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchTrendingBlogs = async () => {
+    try {
+      const {
+        data: { blogs },
+      } = await axios.get(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/trending-blogs`
+      );
+
+      setTrendingBlogs(blogs);
     } catch (error) {
       console.log(error);
     }
@@ -22,6 +38,7 @@ const HomePage = () => {
 
   useEffect(() => {
     fetchLatestBlogs();
+    fetchTrendingBlogs();
   }, []);
 
   return (
@@ -33,25 +50,38 @@ const HomePage = () => {
             routes={["home", "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
-            <>
-              {blogs === null ? (
-                <Loader />
-              ) : (
-                blogs.map((blog, index) => {
-                  return (
-                    <AnimationWrapper
-                      key={index}
-                      transition={{ duration: 1, delay: index * 0.1 }}
-                    >
-                      <BlogPostCard
-                        blog={blog}
-                        author={blog.author.personalInfo}
-                      />
-                    </AnimationWrapper>
-                  );
-                })
-              )}
-            </>
+            {latestBlogs === null ? (
+              <Loader />
+            ) : (
+              latestBlogs.map((blog, index) => {
+                return (
+                  <AnimationWrapper
+                    key={index}
+                    transition={{ duration: 1, delay: index * 0.1 }}
+                  >
+                    <BlogPostCard
+                      blog={blog}
+                      author={blog.author.personalInfo}
+                    />
+                  </AnimationWrapper>
+                );
+              })
+            )}
+
+            {trendingBlogs === null ? (
+              <Loader />
+            ) : (
+              trendingBlogs.map((blog, index) => {
+                return (
+                  <AnimationWrapper
+                    key={index}
+                    transition={{ duration: 1, delay: index * 0.1 }}
+                  >
+                    <MinimalBlogPost blog={blog} index={index} />
+                  </AnimationWrapper>
+                );
+              })
+            )}
           </InPageNavigation>
         </div>
 
