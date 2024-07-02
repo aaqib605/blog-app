ea2dcae54d4f27769c0f9558f7ddc5bc37301363
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-07-02 14:49:37 +0530

    feat: display latest blogs on UI

--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
+import BlogPostCard from "../components/blog-post.component";
 import Loader from "../components/loader.component";
 
 const HomePage = () => {
@@ -36,7 +37,19 @@ const HomePage = () => {
               {blogs === null ? (
                 <Loader />
               ) : (
-                blogs.map((blog, index) => <h1 key={index}>{blog.title}</h1>)
+                blogs.map((blog, index) => {
+                  return (
+                    <AnimationWrapper
+                      key={index}
+                      transition={{ duration: 1, delay: index * 0.1 }}
+                    >
+                      <BlogPostCard
+                        blog={blog}
+                        author={blog.author.personalInfo}
+                      />
+                    </AnimationWrapper>
+                  );
+                })
               )}
             </>
           </InPageNavigation>
